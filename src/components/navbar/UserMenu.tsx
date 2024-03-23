c6f50b75c130c0b4a69833adde196961c4006881
anthony.williams <anthony.williams@qcells.com>
2024-03-23 11:57:09 -0400

    Added logged in user menu options

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -6,11 +6,13 @@ import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "../../hooks/useRegisterModal";
 import useLoginModal from "../../hooks/useLoginModal";
+import { useAuth } from "../../context/auth";
 
 const UserMenu = () => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const [isOpen, setIsOpen] = useState(false);
+  const { user, logout } =  useAuth();
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -73,7 +75,36 @@ const UserMenu = () => {
             text-sm
         ">
           <div className="flex flex-col cursor-pointer">
-            <>
+            {user ? (
+              <>
+              <MenuItem 
+                onClick={() => {}}
+                label="My trips"
+              />
+              <MenuItem 
+                onClick={() => {}}
+                label="My favorites"
+              />
+              <MenuItem 
+                onClick={() => {}}
+                label="My reservations"
+              />
+              <MenuItem 
+                onClick={() => {}}
+                label="My properties"
+              />
+              <MenuItem 
+                onClick={() => {}}
+                label="Airbnb my home"
+              />
+              <hr/>
+              <MenuItem 
+                onClick={async () => {await logout();}}
+                label="Logout"
+              />
+            </>
+            ): (
+              <>
               <MenuItem 
                 onClick={loginModal.onOpen}
                 label="Login"
@@ -83,6 +114,7 @@ const UserMenu = () => {
                 label="Sign up"
               />
             </>
+            )}
           </div>
         </div>
       )}
