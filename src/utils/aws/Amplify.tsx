270b0c54d099f41eb2da6e3d7325f33ad15d7512
anthony.williams <anthony.williams@qcells.com>
2024-04-06 14:10:11 -0400

    added preparation for social logins

--- src/utils/aws/Amplify.tsx
+++ src/utils/aws/Amplify.tsx
@@ -7,6 +7,16 @@ Amplify.configure({
       userPoolClientId: process.env.REACT_APP_AWS_COGNITO_APP_CLIENT_ID,
       allowGuestAccess: true,
       identityPoolId: process.env.REACT_APP_AWS_COGNITO_GUEST_IDENTITY_POOL_ID,
+      loginWith: {
+        oauth: {
+          domain: '',
+          scopes: ['email', 'openid', 'profile',],
+          providers: ['Facebook', 'Google'],
+          redirectSignIn: ['http://localhost:3000/'],
+          redirectSignOut: ['http://localhost:3000/'],
+          responseType: 'token',
+        }
+      }
     }
   },
   API: {
