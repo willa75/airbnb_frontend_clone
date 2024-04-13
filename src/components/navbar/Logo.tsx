903f39921c982d3866387a1359fd5868e99f6053
anthony.williams <anthony.williams@qcells.com>
2024-04-13 08:48:09 -0400

    added link to home page on logo

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,16 +1,19 @@
 'use client';
 
+import { Link } from "react-router-dom";
 import { Image } from "../Image/Image";
 
 const Logo = () => {
   return (
-    <Image
-      alt='logo'
-      className='hidden md:block cursor-pointer'
-      height="100"
-      width="100"
-      src="/images/logo.png"
-    />
+    <Link to={"/"}>
+      <Image
+        alt='logo'
+        className='hidden md:block cursor-pointer'
+        height="100"
+        width="100"
+        src="/images/logo.png"
+      />
+    </Link>
   );
 };
 
